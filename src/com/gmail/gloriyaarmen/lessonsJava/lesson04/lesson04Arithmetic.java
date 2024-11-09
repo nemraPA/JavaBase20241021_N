9f2c2f182a7d6318ccfbcf3b18088ac1066c2694
nemraPA <gloriyaarmen@gmail.com>
2024-11-09 20:01:11 +0200

    lesson04Arithmetic added number 77 to the line

--- src/com/gmail/gloriyaarmen/lessonsJava/lesson04/lesson04Arithmetic.java
+++ src/com/gmail/gloriyaarmen/lessonsJava/lesson04/lesson04Arithmetic.java
@@ -3,6 +3,7 @@ package com.gmail.gloriyaarmen.lessonsJava.lesson04;
 public class lesson04Arithmetic {
 
     public static void main(String[] args) {
-        System.out.println("Arithmetic_N");
+        int test01 = 77;
+        System.out.println("Arithmetic_N " + test01);
     }
 }
