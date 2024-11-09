d539f12d289ecd908935b6d67f2833f9b6d0d8ef
nemraPA <gloriyaarmen@gmail.com>
2024-11-09 19:31:39 +0200

    lesson04Arithmetic first commit

--- src/com/gmail/gloriyaarmen/lessonsJava/lesson04/lesson04Arithmetic.java
+++ src/com/gmail/gloriyaarmen/lessonsJava/lesson04/lesson04Arithmetic.java
@@ -3,6 +3,6 @@ package com.gmail.gloriyaarmen.lessonsJava.lesson04;
 public class lesson04Arithmetic {
 
     public static void main(String[] args) {
-        System.out.println("Arithmetic");
+        System.out.println("Arithmetic_N");
     }
 }
