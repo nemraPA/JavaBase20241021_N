955e87574988524b4cb6deb7b4590acae6f4b61c
nemraPA <gloriyaarmen@gmail.com>
2024-11-10 19:54:09 +0200

    lesson05 ConditionDemo double "єнд" and "ор" lesson 05

--- src/com/gmail/gloriyaarmen/lessonsJava/lesson05/lesson05ConditionDemo.java
+++ src/com/gmail/gloriyaarmen/lessonsJava/lesson05/lesson05ConditionDemo.java
@@ -3,16 +3,16 @@ package com.gmail.gloriyaarmen.lessonsJava.lesson05;
 public class lesson05ConditionDemo {
     public static void main(String[] args) {
         int result;
-        int a = 12;
+        int a = 11;
         int b = 12;
 
-        if ( (a % 2 == 0) & (b % 2 == 0) ){
+        if ( (a % 2 == 0) && (b % 2 == 0) ){
             System.out.println("00");
         } else {
             System.out.println("Unexpected");
         }
 
-        if ( (a % 2 == 0) | (b % 2 == 0) ){
+        if ( (a % 2 == 0) || (b % 2 == 0) ){
             System.out.println("00 or 10 or 01");
         } else {
             System.out.println("Unexpected");
