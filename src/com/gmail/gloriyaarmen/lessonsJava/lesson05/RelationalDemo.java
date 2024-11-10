b2f597fceb467aa98965881cb28f550e562340a1
nemraPA <gloriyaarmen@gmail.com>
2024-11-10 18:27:33 +0200

    lesson05 First commit lesson 05

---
+++ src/com/gmail/gloriyaarmen/lessonsJava/lesson05/RelationalDemo.java
@@ -0,0 +1,15 @@
+package com.gmail.gloriyaarmen.lessonsJava.lesson05;
+
+public class RelationalDemo {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = -123;
+
+        boolean result = a > b;
+        System.out.println("a > b is " + result);
+        result = a < b;
+        System.out.println("a < b is " + result);
+    }
+
+}
