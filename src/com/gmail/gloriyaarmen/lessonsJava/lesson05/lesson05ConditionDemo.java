463ba753af75a6ffe91440378ff47cabe3ce4f42
nemraPA <gloriyaarmen@gmail.com>
2024-11-10 19:27:08 +0200

    lesson05 ConditionDemo commit lesson 05

---
+++ src/com/gmail/gloriyaarmen/lessonsJava/lesson05/lesson05ConditionDemo.java
@@ -0,0 +1,11 @@
+package com.gmail.gloriyaarmen.lessonsJava.lesson05;
+
+public class lesson05ConditionDemo {
+    public static void main(String[] args) {
+        int result;
+        int a = 28;
+        int b = 25;
+
+
+        System.out.println(result);
+}
