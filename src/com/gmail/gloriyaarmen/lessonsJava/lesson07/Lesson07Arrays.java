e610f59c301b64d4c90c663b8d7f3f5ef4c73d04
nemraPAworking <gloriyaarmen@gmail.com>
2024-11-13 20:13:40 +0200

    lesson07 learning arrays just a test

--- src/com/gmail/gloriyaarmen/lessonsJava/lesson07/Lesson07Arrays.java
+++ src/com/gmail/gloriyaarmen/lessonsJava/lesson07/Lesson07Arrays.java
@@ -2,6 +2,10 @@ package com.gmail.gloriyaarmen.lessonsJava.lesson07;
 
 public class Lesson07Arrays {
     public static void main(String[] args) {
+        int i = 7;
+        if(i < 10)
+            System.out.println("1 ");
+            System.out.println("2 ");
 
     }
 }
