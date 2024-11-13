0cbd2f910616065bf8b7d5d06ed0a266c39aa4da
nemraPA <gloriyaarmen@gmail.com>
2024-11-13 22:32:54 +0200

    Lesson06 Homework_06 Correcting spaces and brackets

--- src/com/gmail/gloriyaarmen/lessonsJava/lesson06/Lesson06Homework.java
+++ src/com/gmail/gloriyaarmen/lessonsJava/lesson06/Lesson06Homework.java
@@ -4,11 +4,7 @@ import java.util.Scanner;
 
 public class Lesson06Homework {
     public static void main(String[] args) {
-
-
 //  вычисление при помощи for
-
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter an amount from 1 to 10:");
         int numberEntered = scanner.nextInt();
@@ -20,30 +16,24 @@ public class Lesson06Homework {
         }
         System.out.println("_____________________");
         System.out.printf("Sum of numbers is %d %s %s", result, "\n", "\n");
-
 //  вычисление при помощи while
-
-
         result = 0;
         int i = 1;
-        while(i <= numberEntered) {
+        while (i <= numberEntered) {
             result = result + i;
             System.out.println(i + ") Num is " + i + ", sum is " + result);
             i++;
         }
         System.out.println("_____________________");
         System.out.printf("Sum of numbers is %d %s %s", result, "\n", "\n");
-
-
 //  вычисление при помощи do while
-
         result = 0;
         i = 1;
-        do{
+        do {
             result = result + i;
             System.out.println(i + ") Num is " + i +  ", sum is " + result);
             i++;
-        }while(i <= numberEntered);
+        } while (i <= numberEntered);
         System.out.println("_____________________");
         System.out.printf("Sum of numbers is %d %s %s", result, "\n", "\n");
     }
