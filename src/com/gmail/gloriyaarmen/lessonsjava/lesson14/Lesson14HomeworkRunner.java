5c0d476febcac7bb48800ec75c5944ed2aae71f8
nemraPA <gloriyaarmen@gmail.com>
2024-12-24 06:50:22 +0200

    Lesson14 Homework14 дом пк

---
+++ src/com/gmail/gloriyaarmen/lessonsjava/lesson14/Lesson14HomeworkRunner.java
@@ -0,0 +1,9 @@
+package com.gmail.gloriyaarmen.lessonsjava.lesson14;
+
+public class Lesson14HomeworkRunner {
+    public static void main(String[] args) {
+        ConsolePrinter.Messege[] messege = {
+                new ConsolePrinter.Messege("Santa Clause", "Ho-ho-ho"),
+        }
+    }
+}
