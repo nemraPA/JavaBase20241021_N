c1188bb591b3ad0d9eb4bc3664e71dcd30dc7d92
nemraPAworking <gloriyaarmen@gmail.com>
2024-12-25 17:16:50 +0200

    Lesson15 Homework15 продолжение просмотра занятия

--- src/com/gmail/gloriyaarmen/lessonsjava/lesson15/Status.java
+++ src/com/gmail/gloriyaarmen/lessonsjava/lesson15/Status.java
@@ -1,7 +1,28 @@
 package com.gmail.gloriyaarmen.lessonsjava.lesson15;
 
-public enum Status {
-    TODO,
-    IN_PROGRESS,
-    DONE;
+public enum Status implements Runnable {
+    TODO(9),
+    IN_PROGRESS(47),
+    DONE(1000000) {
+        private static final String MESSEGE = "ALL DONE)";
+        @Override
+        public void run() {
+            System.out.println(MESSEGE);
+        }
+    };
+
+    Status(int defaultDuration) {
+        this.defaultDuration = defaultDuration;
+    }
+
+    private int defaultDuration;
+
+    public int getDefaultDuration() {
+        return defaultDuration;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Running....");
+    }
 }
--- src/com/gmail/gloriyaarmen/lessonsjava/lesson15/TaskDemo.java
+++ src/com/gmail/gloriyaarmen/lessonsjava/lesson15/TaskDemo.java
@@ -1,10 +1,49 @@
 package com.gmail.gloriyaarmen.lessonsjava.lesson15;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class TaskDemo {
     public static void main(String[] args) {
         Task task = new Task(Status.TODO, "Learn Java");
         System.out.println("Primary task is: " + task);
+        System.out.println("Expected duration: " + task.getStatus().getDefaultDuration());
+        System.out.println(" -= IN PROGRESS =- ");
+        task.setStatus(Status.IN_PROGRESS);
+        Status currentStatus = task.getStatus();
+        System.out.println("Current status is " + currentStatus);
+        System.out.println("Expected duration after status was changed: " + task.getStatus().getDefaultDuration());
+        currentStatus.run();
+
+        System.out.println(" -= DONE =- ");
+        task.setStatus(Status.DONE);
+        currentStatus = task.getStatus();
+        System.out.println("Current status is " + currentStatus);
+        System.out.println("Expected duration after status was changed: " + task.getStatus().getDefaultDuration());
+        currentStatus.run();
+
+
+//        if (Status.DONE.equals(currentStatus)) {
+        if (Status.DONE == currentStatus) {
+            System.out.println("Status is DONE!");
+        }
+
+        Status[] availableStatus = Status.values();
+        System.out.println("Available status: " + Arrays.toString(availableStatus));
+        System.out.printf("%s has an ordinal == %d\n", currentStatus.name(), currentStatus.ordinal());
+        Status anoverStatus = Status.valueOf("TODO");
+        System.out.println("Anover status from String is: " + anoverStatus);
+
+        switch (anoverStatus) {
+            case TODO:
+                System.out.println("TODO behaviour");
+                break;
+            default:
+                System.out.println("Unknown  behaviour");
+                break;
+        }
+
 //        task.setStatus(new Task.Status("FAKE STATUS"));   // error! constructor is private
-        System.out.println("Changed status is: " + task.getStatus());
+//        System.out.println("Changed status is: " + task.getStatus());
     }
 }
